dab23adad80b70499855cbddc3c8a562b6ecdf82
yassine grairi <grairiiyassine@gmail.com>
2026-06-19 22:59:36 +0100

    feat: add auth middleware with role-based guards

---
+++ middlware/auth.php
@@ -0,0 +1,36 @@
+<?php
+
+function isGuest(): bool{
+    return empty($_SESSION["user"]);
+}
+
+function isUser(): bool{
+    return !empty($_SESSION["user"]) && $_SESSION["user"]["role"] === "user";
+}
+
+function isAdmin(): bool{
+    return !empty($_SESSION["user"]) && $_SESSION["user"]["role"] === "admin";
+}
+
+
+
+function guestOnly():void{
+    if (!empty($_SESSION["user"])){
+    header("Location: /?route=home");
+    exit();
+    }
+}
+
+function usersOnly():void{
+    if (empty($_SESSION["user"]) || !empty($_SESSION["user"]) && $_SESSION["user"]["role"] === "admin"){
+    header("Location: /?route=login");
+    exit();
+    }
+}
+
+function adminOnly():void{
+    if (empty($_SESSION["user"]) || !empty($_SESSION["user"]) && $_SESSION["user"]["role"] === "user"){
+    header("Location: /?route=login");
+    exit();
+    }
+}
