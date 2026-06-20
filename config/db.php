f8b6b067808ad48c93f488799447951f0119047f
yassine grairi <grairiiyassine@gmail.com>
2026-06-20 03:07:05 +0100

    fix: correct PDO DSN parameter syntax

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 
 try{
-    $pdo = new PDO("mysql:host=localhost;dbname=gestion_contacts_companies;root,fmcharmal");
+    $pdo = new PDO("mysql:host=localhost;dbname=gestion_contacts_companies","root","fmcharmal");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 }catch (PDOException $e){
