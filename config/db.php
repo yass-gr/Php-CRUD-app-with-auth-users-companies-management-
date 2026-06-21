4d2af21da2da6d15e2768125a0c7e71e7f4a8a0e
yassine grairi <grairiiyassine@gmail.com>
2026-06-22 00:22:45 +0100

    add gitignore and move db credentials to env

--- config/db.php
+++ config/db.php
@@ -1,8 +1,9 @@
 <?php
 
+$env = parse_ini_file(__DIR__ . "/../.env");
 
 try{
-    $pdo = new PDO("mysql:host=localhost;dbname=gestion_contacts_companies","root","fmcharmal");
+    $pdo = new PDO("mysql:host={$env["DB_HOST"]};dbname={$env["DB_NAME"]}", $env["DB_USER"], $env["DB_PASS"]);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 }catch (PDOException $e){
